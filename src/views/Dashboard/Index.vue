c2c965ef530f5cd3acba67e40c20b44912fee4f3
siler_shen <spx123456@foxmail.com>
2019-01-18 09:38:18 +0800

    add clipboard

--- src/views/Dashboard/Index.vue
+++ src/views/Dashboard/Index.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="dashboard-container">
-        <img alt="stream" :src="imgSrc"/>
+    <div class="app-container">
+        <el-input v-model="currentRole" placeholder="Please input" style="width:400px;max-width:100%;"/>
+        <el-button type="primary" icon="document" @click="handleCopy(currentRole, $event)">copy</el-button>
     </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import clipboard from '@/utils/clipboard'
 
   export default {
     name: 'Dashboard',
@@ -24,6 +26,11 @@
     created () {
       if (!this.roles.includes('admin')) {
         this.currentRole = 'editorDashboard'
+      }
+    },
+    methods: {
+      handleCopy (text, event) {
+        clipboard(text, event)
       }
     }
   }
